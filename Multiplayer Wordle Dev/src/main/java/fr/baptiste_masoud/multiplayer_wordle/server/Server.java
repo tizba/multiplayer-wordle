62db5c6d7095c26bfe1f47cfa4859474aec4332b
Baptiste Masoud <66852473+tizba@users.noreply.github.com>
2022-04-27 12:12:08 +0200

    Fixed Sonarlint's warnings and added random word

--- Multiplayer Wordle Dev/src/main/java/fr/baptiste_masoud/multiplayer_wordle/server/Server.java
+++ Multiplayer Wordle Dev/src/main/java/fr/baptiste_masoud/multiplayer_wordle/server/Server.java
@@ -2,51 +2,66 @@ package fr.baptiste_masoud.multiplayer_wordle.server;
 
 import fr.baptiste_masoud.multiplayer_wordle.messages.s_to_c.TooManyPlayersMessage;
 import fr.baptiste_masoud.multiplayer_wordle.server.game.Game;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
+
 public class Server {
     private Game game;
-    private boolean running;
     private final int port;
+    private static final Logger serverLogger = LogManager.getLogger(Server.class);
+    private final List<String> words;
 
-    public Server(int port) {
+    public Server(int port) throws IOException {
         this.port = port;
+        this.words = initWords();
+    }
+
+    private List<String> initWords() throws IOException {
+        List<String> localWordList = new ArrayList<>();
+        InputStream inputStream = Server.class.getClassLoader().getResourceAsStream("liste_mots.txt");
+        if (inputStream == null) {
+            throw new FileNotFoundException("liste_mots.txt not found in resources folder");
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            localWordList.add(line);
+        }
+        return localWordList;
     }
 
     /**
      * Starts the server, it is now listening on his port and waiting for new client connections to handle them
+     *
      * @throws IOException if the server is unable to listen on his port
-     * or if an I/O error occurs while waiting for clients
+     *                     or if an I/O error occurs while waiting for clients
      */
     public void start() throws IOException {
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Listening on port " + serverSocket.getLocalPort());
-        running = true;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            serverLogger.info("Listening on port {}", serverSocket.getLocalPort());
+            Boolean running = true;
 
-        Thread userInputThread = new Thread(() -> {
-            Scanner scanner = new Scanner(System.in);
-            while (running) {
-                String input = scanner.next();
-                if (Objects.equals(input.toUpperCase(), "STOP")) {
-                    running = false;
-                    System.exit(0);
+            new Thread(new CheckUserInputForStop(running)).start();
+
+            //noinspection UnnecessaryUnboxing,ConstantConditions
+            while (running.booleanValue()) {
+                Socket clientSocket = serverSocket.accept();
+                if (this.game == null) {
+                    initNewGame();
                 }
+                handleNewClient(clientSocket);
             }
-        });
-
-        userInputThread.start();
-
-        while (running) {
-            Socket clientSocket = serverSocket.accept();
-            if (this.game == null) {
-                initNewGame();
-            }
-            handleNewClient(clientSocket);
         }
     }
 
@@ -54,8 +69,26 @@ public class Server {
      * Initializes a new game
      */
     public void initNewGame() {
-        System.out.println("New game !");
-        this.game = new Game(this);
+        serverLogger.info("New game !");
+        this.game = new Game(this, words);
+    }
+
+    private static class CheckUserInputForStop implements Runnable {
+        private Boolean running;
+
+        public CheckUserInputForStop(Boolean running) {
+            this.running = running;
+        }
+
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+            while (Boolean.TRUE.equals(running)) {
+                String input = scanner.next();
+                if (Objects.equals(input.toUpperCase(), "STOP")) {
+                    running = false;
+                }
+            }
+        }
     }
 
     /**
--- Multiplayer Wordle Dev/src/test/java/TodoTest.java
+++ Multiplayer Wordle Dev/src/test/java/TodoTest.java
@@ -1,14 +1,15 @@
-import fr.baptiste_masoud.multiplayer_wordle.server.Server;
-import fr.baptiste_masoud.multiplayer_wordle.server.game.Game;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TodoTest {
+class TodoTest {
 
     @Test
-    public void Test() {
+    void Test() {
         //Todo: Test
-        Game game = new Game(new Server(1234));
-        assertEquals(game,game);
+        String s = "TODO";
+        String s2 = "TOD";
+        s2 += "O";
+        assertEquals(s, s2);
     }
 }
