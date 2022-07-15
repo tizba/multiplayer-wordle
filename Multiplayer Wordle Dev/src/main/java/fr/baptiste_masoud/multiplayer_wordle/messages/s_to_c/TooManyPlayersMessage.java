e2491921f69a69d4aa8613987f346de20675f97e
Baptiste Masoud <baptiste.masoud@gmail.com>
2022-07-15 10:20:46 +0200

    Change project name and folder name

--- Multiplayer Wordle Dev/src/main/java/fr/baptiste_masoud/multiplayer_wordle/messages/s_to_c/TooManyPlayersMessage.java
+++
@@ -1,7 +0,0 @@
-package fr.baptiste_masoud.multiplayer_wordle.messages.s_to_c;
-
-public class TooManyPlayersMessage extends ServerToClientMessage {
-    public TooManyPlayersMessage() {
-        super(ServerToClientMessageType.TOO_MANY_PLAYERS);
-    }
-}
--- Multiplayer Wordle Dev/src/main/java/fr/baptiste_masoud/multiplayer_wordle/server/game/GameIsFullException.java
+++
@@ -1,8 +0,0 @@
-package fr.baptiste_masoud.multiplayer_wordle.server.game;
-
-class GameIsFullException extends RuntimeException {
-
-    public GameIsFullException(String message) {
-        super(message);
-    }
-}
